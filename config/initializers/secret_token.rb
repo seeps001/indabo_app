e82d11932547e0a99da2610ee37afad90a397e28
Raoul de Villiers <raouldevilliers@gmail.com>
2012-03-24 12:41:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IndaboApp::Application.config.secret_token = '0e9ff6a327ed4d9842247132b43a5ac3cc8ebb181c92e2ae379e6bf3e75fa0a9bb35aa41e9a8156179b4b2b9f007cc2d06009b519a2abd5f15508ecd94af4f1c'
